c3da822f556293955938503ffacbda7a35f0c050
RICK SILVA <henriguedes34@gmail.com>
2024-10-28 22:33:15 -0300

    ex1 arraylist

---
+++ arraylist1/src/main/java/com/mycompany/arraylist1/Arraylist1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraylist1;
+
+/**
+ *
+ * @author h8s5g
+ */
+
+import java.util.ArrayList;
+
+public class Arraylist1 {
+
+public static void main(String[] args) {
+    
+        ArrayList<String> frutas = new ArrayList<>();
+
+        frutas.add("Maçã");
+        frutas.add("Banana");
+        frutas.add("Laranja");
+        frutas.add("Manga");
+        frutas.add("Uva");
+
+        System.out.println("Lista de Frutas: " + frutas);
+    }
+
+}
